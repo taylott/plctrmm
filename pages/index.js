e13b19e7dd50370af7238f948c89b583e72fd280
Tom Aylott <tomaylott@gmail.com>
2021-06-20 21:21:33 +0100

    adjust oembed removal

--- pages/index.js
+++ pages/index.js
@@ -2,37 +2,55 @@ import Head from 'next/head'
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import ComponentLoader from '../components/ComponentLoader'
-import {extract} from 'oembed-parser'
 import Theme from './Theme.js'
+import useSWR from 'swr'
 
-export default function Home({ data, embeds }) {
+const fetcher = async ( url ) => {
+  const res = await fetch(url)
+  const obj = await res.json()
+
+  if (!obj) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return  {obj}
+}
+
+
+export default function Home({ apiBase, site, slug }) {
+
+  const dataURL = `${apiBase}/${site}/${slug}`
+
+  const {data} = useSWR(dataURL, fetcher)
+
+  if (!data) return <div className="container text-center">loading...</div>
 
   return (
     <div className="wrap">
       <Head>
-        <title>{data.title}</title>
+        <title>{data.obj.title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-        <Header/>
-        <main className="site-content w-full mx-auto my-5 rounded-md overflow-hidden">
-            <div> 
-              {
-                data.content.map( (block, index) => {
-                    if (block.blockType == 'mediaPlayer') {
-                      block.blockEmbed = embeds[block.blockID]
-                    }
-                    return <ComponentLoader key={ 'block_' + index } block={block}/>
-                  }
-                )}
-            </div>
-        </main>
-        <Footer/>
-        <Theme theme={data.theme}/>
+
+      <Header />
+      <main className="site-content w-full mx-auto my-5 rounded-md overflow-hidden">
+        <div>
+          {
+            data.obj.content.map((block, index) => {
+
+              return <ComponentLoader key={'block_' + index} block={block} />
+              
+            }
+            )}
+        </div>
+      </main>
+      <Footer />
+      <Theme theme={data.obj.theme} />
     </div>
   )
 }
-
 
 export async function getServerSideProps(context) {
 
@@ -54,32 +72,12 @@ export async function getServerSideProps(context) {
   const slug = context.query.slug ?? '__home__'
   const apiBase = process.env.API_BASE
 
-
-  //do some request magic
-  const res = await fetch(`${apiBase}/${site}/${slug}`)
-  const data = await res.json()
-  if (!data) {
-    return {
-      notFound: true,
-    }
-  }
-  
-
-  let embeds = {};
-
-  if (data.oembeds) {
-    for (let i = 0; i < data.oembeds.length; i++) { 
-      let o = await extract(data.oembeds[i].source).then((oembed) => {
-        return oembed
-      }).catch((err) => {
-        console.trace(err)
-      });
-      let assoc = data.oembeds[i].assoc;
-      embeds[assoc] = o
-    }
-  }
-
   return {
-    props: {data, embeds}, // will be passed to the page component as props
+    props: {
+      slug: slug,
+      apiBase: apiBase,
+      site: site
+    }
   }
+
 }
